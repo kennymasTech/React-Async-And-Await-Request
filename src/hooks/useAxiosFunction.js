2753760cefd3173b2dbfa883bcdad91383e5438e
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-21 08:38:37 +0100

    a

--- src/hooks/useAxiosFunction.js
+++ src/hooks/useAxiosFunction.js
@@ -1,38 +1,39 @@
 import { useState, useEffect } from "react";
 
 const useAxiosFunction = () => {
-    
-
     const [response, setResponse] = useState([]);
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
     const [controller, setController] = useState();
 
     const axiosFetch = async (configObj) => {
-        const { axiosInstance, method, url, requestConfig = {} } = configObj;
+        const { 
+            axiosInstance, 
+            method, 
+            url, 
+            requestConfig = {} 
+        } = configObj;
+
+        try {
+            const res = await axiosInstance[method.toLowerCase()](url, {
+                ...requestConfig,
+                // signal: controller.signal
+            })
+            console.log(res);
+            isMounted && setResponse(res.data)
+
+        } catch (err) {
+            console.log(err);
+            setError(err.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
     useEffect(() => {
         let isMounted = true;
         // const controller = new AbortController();
-
-        const fetchData = async () => {
-            try {
-                const res = await axiosInstance[method.toLowerCase()](url, {
-                    ...requestConfig,
-                    // signal: controller.signal
-                })
-                console.log(res);
-                isMounted && setResponse(res.data)
-
-            } catch (err) {
-                console.log(err);
-                setError(err.message)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchData();
+        
 
         return () => {
             isMounted = false
