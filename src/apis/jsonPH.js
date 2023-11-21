b3916a08531ebf7a4b3b45ff8a2bfd9edc4ac653
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-21 08:58:43 +0100

    a

--- src/apis/jsonPH.js
+++ src/apis/jsonPH.js
@@ -1,5 +1,6 @@
+
 import axios from "axios";
-const BASE_URL = "https://jsonplaceholder.typicode.com/";
+const BASE_URL = "https://jsonplaceholder.typicode.com";
 
 export default axios.create({
     baseURL: BASE_URL,
