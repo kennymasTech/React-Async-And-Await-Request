6c5a554298f60eb67897c9f64794332fbb3161af
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-21 12:01:23 +0100

    a

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,58 +1,126 @@
+
+import { useEffect } from "react";
 import axios from "../apis/jsonPH";
 import useAxiosFunction from "../hooks/useAxiosFunction";
-import { useEffect } from "react";
-import React from 'react'
-
 
 const Posts = () => {
-    const [posts, loading, error, axiosFetch] = useAxiosFunction();
+   const [posts, loading, error, axiosFetch] = useAxiosFunction();
+   const getData = () => {
+      axiosFetch({
+         axiosInstance: axios,
+         url: "/posts",
+         method: "GET",
+      });
+   };
 
-    useEffect (() => {
-        // to get the data from the endpoint
-        axiosFetch ({
-            axiosInstance: axios,
-            method: "GET",
-            url: "/posts",
-        })
-    }, [])
+   useEffect(() => {
+      getData();
+   }, []);
 
-    const handleSubmit = () => {
-        axiosFetch ({
-            axiosInstance: axios,
-            method: "post",
-            url: "/posts",
-            requestConfig: {
-                data: {
-                    userId: 10,
-                    title: "Kennymas",
-                    body: "brother oni chicken...",
-                },
+   const handleSubmit = () => {
+      axiosFetch({
+         axiosInstance: axios,
+         url: "/posts",
+         method: "POST",
+         requestConfig: {
+            data: {
+               userId: 10,
+               title: "Supreme",
+               body: "Holla at your boy...",
             },
-        })
-    }
+         },
+      });
+   };
 
-  return <article>
-            <h2>Posts</h2>
+   return (
+      <article>
+         <h2>Posts</h2>
+         <div className="row">
+            <button onClick={handleSubmit}>Submit</button>
+         </div>
 
-            <div className="row">
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
+         {loading && <p>Loading...</p>}
+         {!loading && error && <p className="errMsg">{error}</p>}
+         {!loading && !error && posts?.length && (
+            <ul>
+                {
+                    posts.map((post, i) => (
+                        <li key={i}>{post.id}. {post.title}</li>
+                    ))
+                }
+            </ul>
+         )}
+         {!loading && !error && posts?.data && 
+            <p>
+                userId: {posts.data?.userId},  title: {posts.data?.title}, body: {posts.data?.body}
+            </p>
+         }
+         {!loading && !error && !posts && <p>No Post to Display! 😞</p>}
 
-            {loading && <p>loading...</p>}
-            {!loading && error && <p className="errMsg">{error}</p>}
-            {!loading && !error && posts?.length && 
-                <ul>
-                    {posts.map((post, i) => (
-                            <li key={i}>{post.id}. {post.title}</li>
-                        ))}
-                </ul> 
-            }
-            {!loading && !error && posts?.length && posts?.data && (
-                <p>userId:{posts.data?.userId}, title: {posts.data?.title}, body: {posts.data?.body}</p>
-            )}
-            {!loading && !error && !posts && <p>No Post To Display! 🙂</p>}
+      </article>
+   );
+};
 
-         </article>
-}
+export default Posts;
 
-export default Posts
+
+
+
+// import axios from "../apis/jsonPH";
+// import useAxiosFunction from "../hooks/useAxiosFunction";
+// import { useEffect } from "react";
+// import React from 'react'
+
+
+// const Posts = () => {
+//     const [posts, loading, error, axiosFetch] = useAxiosFunction();
+
+//     useEffect (() => {
+//         // to get the data from the endpoint
+//         axiosFetch ({
+//             axiosInstance: axios,
+//             method: "GET",
+//             url: "/posts",
+//         })
+//     }, [])
+
+//     const handleSubmit = () => {
+//         axiosFetch ({
+//             axiosInstance: axios,
+//             method: "post",
+//             url: "/posts",
+//             requestConfig: {
+//                 data: {
+//                     userId: 10,
+//                     title: "Kennymas",
+//                     body: "brother oni chicken...",
+//                 },
+//             },
+//         })
+//     }
+
+//   return <article>
+//             <h2>Posts</h2>
+
+//             <div className="row">
+//                 <button onClick={handleSubmit}>Submit</button>
+//             </div>
+
+//             {loading && <p>loading...</p>}
+//             {!loading && error && <p className="errMsg">{error}</p>}
+//             {!loading && !error && posts?.length && 
+//                 <ul>
+//                     {posts.map((post, i) => (
+//                             <li key={i}>{post.id}. {post.title}</li>
+//                         ))}
+//                 </ul> 
+//             }
+//             {!loading && !error && posts?.length && posts?.data && (
+//                 <p>userId:{posts.data?.userId}, title: {posts.data?.title}, body: {posts.data?.body}</p>
+//             )}
+//             {!loading && !error && !posts && <p>No Post To Display! 🙂</p>}
+
+//          </article>
+// }
+
+// export default Posts
