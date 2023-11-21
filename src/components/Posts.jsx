0b7ed50a33db4a92a66ce25448a9672cdd57606e
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-21 09:01:52 +0100

    a

---
+++ src/components/Posts.jsx
@@ -0,0 +1,15 @@
+import axios from "../apis/jsonPH";
+import useAxiosFunction from "../hooks/useAxiosFunction";
+import { useEffect } from "react";
+
+
+import React from 'react'
+
+const Posts = () => {
+    
+  return (
+    <div>Posts</div>
+  )
+}
+
+export default Posts
